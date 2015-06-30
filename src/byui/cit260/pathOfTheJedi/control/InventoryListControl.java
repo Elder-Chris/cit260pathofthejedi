751200084bf7ec095a9ca0453d405554556d5e23
Kika <Kika@192.168.0.122>
2015-06-30 17:29:39 -0500

    6/30/2015 - Array sumList on Inventory Control and Inventory list

--- src/byui/cit260/pathOfTheJedi/control/InventoryListControl.java
+++ src/byui/cit260/pathOfTheJedi/control/InventoryListControl.java
@@ -55,7 +55,7 @@ public class InventoryListControl {
     public static void sumList() {
         
         ArrayList<InventoryList> al = new ArrayList<>();
-        al.add(new InventoryList("blue", 5));
+        al.add(new InventoryList("blue", 6));
         al.add(new InventoryList("green", 2));
         al.add(new InventoryList("yellow", 1));
         al.add(new InventoryList("black", 4));
